b8a44cd2ad62188b553252ea63647a416dc87df3
mackbowes <mackenziepbowes@gmail.com>
2021-09-26 17:48:59 -0700

    solved async api call issue, md now renders

--- pages/renderTest.js
+++ pages/renderTest.js
@@ -1,36 +1,51 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
 
-export default function renderTest(markdown) {
-  const getData = async () => {
-    // membership = getMember();
-    const membership = true;
-    let body = { membership };
-    body = JSON.stringify(body);
+export default function renderTest() {
+	/**
+	 * getData is an async function to 
+	 * a) check status of the user's current metamask/web3 provider address
+	 * b) compare that to a dictionary of DAO member address
+	 * c) if the current web3 provider address matches,
+	 * d) call an API that returns something.
+	 * 
+	 * @returns A secure result from the server
+	 */
+	const getData = async () => { // store an anonymous asynchronous function into a variable called getData;
+		// getData is *just* a variable, to trigger it we need to call await getData(); inside of another async function
+		// membership = getMember();
+		const membership = true;
+		let body = { membership };
+		body = JSON.stringify(body);
 
-    let myHeaders = new Headers();
-    const myInit = {
-      method: 'POST',
-      headers: myHeaders,
-      mode: 'cors',
-      cache: 'default',
-      body,
-      membership: true,
-    };
-    const url = '/api/test';
-    const response = await fetch(url, myInit);
-    const resJson = await response.json();
-    const theNumberFive = 5;
-    console.log(theNumberFive);
-    return theNumberFive;
-  };
+		let myHeaders = new Headers();
+		const myInit = {
+			method: 'POST',
+			headers: myHeaders,
+			mode: 'cors',
+			cache: 'default',
+			body,
+			membership: true,
+		};
+		const url = '/api/test';
+		const response = await fetch(url, myInit);
+		const resJson = await response.json();
 
-  markdown = 'Placeholder';
+		// console.log(resJson);
+		return resJson;
+	};
 
-  return (
-    <div>
-      {<ReactMarkdown children={markdown} remarkPlugins={[remarkGfm]} />}
-    </div>
-  );
+	const [pageData, setPageData] = useState();
+
+	useEffect(async () => {
+		const returnedData = await getData();
+		setPageData(returnedData.msg);
+	})
+
+	return (
+		<div>
+			<ReactMarkdown children={pageData} remarkPlugins={[remarkGfm]} />
+		</div>
+	);
 }

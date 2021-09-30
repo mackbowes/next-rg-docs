a63e197f3df5fbe91f9c2c941957e15add418887
derrekcoleman <80121818+derrekcoleman@users.noreply.github.com>
2021-09-30 15:01:27 -0700

    added getMember in an initial form

--- pages/index.js
+++ pages/index.js
@@ -3,24 +3,35 @@ import styles from '../styles/Home.module.css';
 import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
 import { PageWrapper } from '../components/PageWrapper';
+import getMember from './api/getMember';
+import { useState, useEffect } from 'react';
 
 import { getPublicDocs } from '../lib/posts';
 
 export default function Home(props) {
-	return (
-		<>
-			<PageWrapper>
-				<ReactMarkdown children={props.allPostsData[0].fileContent} remarkPlugins={[remarkGfm]} />
-			</PageWrapper>
-		</>
-	)
+  const [member, setMember] = useState(0);
+
+  useEffect(async function () {
+    setMember(await getMember());
+  }, []);
+
+  return (
+    <>
+      <PageWrapper>
+        <ReactMarkdown
+          children={props.allPostsData[0].fileContent}
+          remarkPlugins={[remarkGfm]}
+        />
+      </PageWrapper>
+    </>
+  );
 }
 
 export async function getStaticProps() {
-	const allPostsData = getPublicDocs();
-	return {
-		props: {
-			allPostsData,
-		},
-	};
+  const allPostsData = getPublicDocs();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
 }

a82445a7945d25ae123065ec903ec6e8d6ceec2a
derrekcoleman <80121818+derrekcoleman@users.noreply.github.com>
2021-09-30 15:43:33 -0700

    Added key attribute

--- components/PageWrapper.js
+++ components/PageWrapper.js
@@ -1,36 +1,36 @@
 import styles from './styles/PageWrapper.module.scss';
 
 export const PageWrapper = (props) => {
-
-	return (
-		<div className={styles.mainWrapper}>
-			<Sidebar data={props.sidebarData} />
-			<div data='spacer'></div>
-			<div className={styles.pageWrapper}>
-				{props.children}
-			</div>
-		</div>
-	)
-}
+  return (
+    <div className={styles.mainWrapper}>
+      <Sidebar data={props.sidebarData} />
+      <div data="spacer"></div>
+      <div className={styles.pageWrapper}>{props.children}</div>
+    </div>
+  );
+};
 
 const Sidebar = (props) => {
-	const data = props.data;
-	console.log(data);
-	return (
-		<div className={styles.sidebar}>
-			{Object.keys(data).map((category) => {
+  const data = props.data;
+  console.log(data);
+  return (
+    <div className={styles.sidebar}>
+      {Object.keys(data).map((category, index) => {
+        // todo: logic to split CamelCased words
 
-				// todo: logic to split CamelCased words
-
-				return (
-					<>
-						<h2>{category}</h2>
-						{data[category].map((item, index) => {
-							return <a href={`/${category}/${item.slug}`}>{item.title}</a>
-						})}
-					</>
-				)
-			})}
-		</div>
-	)
-}
+        return (
+          <>
+            <h2 key={index}>{category}</h2>
+            {data[category].map((item, index) => {
+              return (
+                <a key={index} href={`/${category}/${item.slug}`}>
+                  {item.title}
+                </a>
+              );
+            })}
+          </>
+        );
+      })}
+    </div>
+  );
+};

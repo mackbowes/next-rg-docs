1927fa542110418df84b6b8919724e4e926dd468
derrekcoleman <80121818+derrekcoleman@users.noreply.github.com>
2021-09-30 15:44:17 -0700

    Fixed broken importing within index.js and [slug].js

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,10 @@
 import { useEffect, useState } from 'react';
-import useSWR from 'swr'
 import Head from 'next/head';
 import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
 import { PageWrapper } from '../components/PageWrapper';
 import getMember from './api/getMember';
-import { useState, useEffect } from 'react';
-
+import { getPublicDocs } from '../lib/posts';
 
 export default function Home(props) {
   const [member, setMember] = useState(0);
@@ -35,4 +33,3 @@ export async function getStaticProps() {
     },
   };
 }
-

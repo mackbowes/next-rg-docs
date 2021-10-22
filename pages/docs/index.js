fe5ad28bd0c31fdbbb5d821db69900217464d054
mackbowes <mackenziepbowes@gmail.com>
2021-10-21 19:36:49 -0700

    pushing wip

--- pages/docs/index.js
+++ pages/docs/index.js
@@ -1,10 +1,8 @@
 import { useState, useEffect } from 'react';
-import { Sidebar } from '../../components/PageWrapper';
-import { getSignature } from '../../lib/getSignature';
-import { getPrivateData } from '../../lib/getPrivateData';
+import { Sidebar, MobileSidebar } from '../../components/PageWrapper';
+import LogInButton from '../../components/LogIn';
+import { MobileSidebarButton } from '../../components/MobileSidebarButton';
 import { Center, Heading, Box, VStack, Grid } from '@chakra-ui/react'
-import { ethers } from "ethers";
-import Head from 'next/head';
 
 export default function Page() {
 
@@ -12,23 +10,27 @@ export default function Page() {
 	const [privatePageData, setPrivatePageData] = useState();
 	const [currentPageData, setCurrentPageData] = useState();
 	const [currentPageContent, setCurrentPageContent] = useState();
+	const [windowWidth, setWindowWidth] = useState();
 
-	useEffect(async () => {
-		const response = await fetch('/api/getPublicDocs');
-		const resJson = await response.json();
-		setPageData(resJson.data);
-		if (!window?.sessionStorage.getItem('privateData') && true) {
-			// get private data IFF it hasn't already been got and the user is allowed
-			const privateResponse = await getPrivateData(await getSignature());
-			const stringifiedPrivateData = JSON.stringify(privateResponse.data);
-			window.sessionStorage.setItem('privateData', stringifiedPrivateData);
-			setPrivatePageData(privateResponse.data);
+	useEffect(() => {
+		const width = window?.innerWidth;
+		console.log(width);
+		setWindowWidth(width);
+		const getData = async () => {	
+			if (!window?.sessionStorage.getItem('publicData')) {
+				const response = await fetch('/api/getPublicDocs');			
+				const resJson = await response.json();
+				const stringifiedPublicData = JSON.stringify(resJson.data);
+				window.sessionStorage.setItem('publicData', stringifiedPublicData);
+				setPageData(resJson.data);
+			}
+			if (window?.sessionStorage.getItem('publicData')) {
+				const stringifiedPublicData = window.sessionStorage.getItem('publicData');
+				const publicData = JSON.parse(stringifiedPublicData);
+				setPageData(publicData);
+			}
 		}
-		if (window?.sessionStorage.getItem('privateData')) {
-			const stringifiedPrivateData = window.sessionStorage.getItem('privateData');
-			const privateData = JSON.parse(stringifiedPrivateData);
-			setPrivatePageData(privateData);
-		}
+		getData();
 	}, []);
 
 	// Set current page to the first item in the PageData array
@@ -41,7 +43,8 @@ export default function Page() {
 
 	return (
 		<>
-			{(typeof pageData !== 'undefined') &&
+			{(typeof pageData !== 'undefined') && (windowWidth > 450) &&
+				<>
 				<Box h="100vh" w="100vw" overflow="hidden" sx={{ position: `relative` }}>
 					<Grid templateColumns="1fr 4fr">
 						<Sidebar data={pageData} />
@@ -53,6 +56,26 @@ export default function Page() {
 						</Center>
 					</Grid>
 				</Box>
+				<LogInButton setPrivatePageData={(d) => setPrivatePageData(d)} /> {/* d for data */}
+				</>
+			}
+			{(typeof pageData !== 'undefined') && (windowWidth <= 450) &&
+				<>
+				<Box h="100vh" w="100vw" overflow="hidden" sx={{ position: `relative` }}>
+					<Grid templateColumns="1fr 4fr">
+						<Sidebar data={pageData} />
+						<Center bg="brand.900" h="100%" w="100%" color="brand.500" sx={{ userSelect: 'none' }}>
+							<VStack align="center" spacing="3rem" sx={{ zIndex: `2` }}>
+								<Heading as="h1" size="3xl" sx={{ textAlign: `center`, textShadow: `0px 4px black` }}>This is a documentation index page.</Heading>
+								<Heading as="h2" size="xl" sx={{ textAlign: `center`, textShadow: `0px 4px black` }}>Use the sidebar to select a doc and start reading.</Heading>		
+							</VStack>
+						</Center>
+					</Grid>
+				</Box>
+				<LogInButton setPrivatePageData={(d) => setPrivatePageData(d)} /> {/* d for data */}
+				<MobileSidebar data={pageData} isVisible={isSidebarVisible} />
+				<MobileSidebarButton onClick={() => setIsSidebarVisible(v => !v)} />
+				</>
 			}
 		</>
 	)
